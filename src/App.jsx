858e2f063fb475e3a9f7d35c493f93c711f4930c
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-29 13:45:48 -0400

    feat(App.jsx): Hooks and API connection added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,25 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 
 const App = () => {
+
+    const [data, setData] = useState(null);
+    
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const fetch = await fetch('');
+            const data = await fetch.json();
+
+            return data
+        };
+
+        setData(fetchData());
+    }, []);
+
     return (
         <>
             <Header />
-            <DisplayStats />
+            <DisplayStats stats={data}/>
             <Footer />
         </>
     )
