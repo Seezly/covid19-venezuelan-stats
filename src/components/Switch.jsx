f007a488b2c290a0bcf3845b0618694a53024725
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-30 11:50:34 -0400

    feat(Switch.jsx): Component added

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -3,8 +3,19 @@ import './switch.css';
 
 const Switch = (props) => {
     return (
-        <>
-        </>
+        <div
+            className="dark-mode switch">
+                <input 
+                    type="checkbox" 
+                    name="check" 
+                    id="check"/>
+                <label 
+                    htmlFor="check"
+                    className="check-trail">
+                    <span 
+                        className="check-handler"/>
+                </label>
+        </div>
     )
 };
 
