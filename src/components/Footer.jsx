09947fa942c2b7fa9d6e5cf700eefdc3f3ea568c
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-30 19:29:52 -0400

    feat(Footer.jsx): Component added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <div
+                className="copyright">
+                <small
+                    className="copyright__data">
+                    Toda la información acerca del SaRS-CoV-2 (COVID-19) mostrada en esta página web es proveída directamente desde la 
+                    <a href="https://covid19.patria.org.ve/" target="_blank" rel="noopener noreferrer">API</a> pública de las estadísticas oficiales del COVID-19 
+                    en Venezuela.
+                </small>
+                <small
+                    className="copyright__author">
+                    Diseñado y desarrollado con <span className="heart">♥</span> por <a href="https://www.sergiogutierrez.ml" target="_blank" rel="noopener noreferrer">Seezly</a>.
+                </small>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
