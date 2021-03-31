268f4380a90a75b93f2aa484123e529a3f0e1cbf
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-31 15:30:13 -0400

    fix(Stats.jsx): span and h2 className fixed

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -8,8 +8,8 @@ const Stats = (props) => {
     return (
         <article
             className={attr ? `${class_name} --${attr}` : `${class_name}`}>
-            <span className={`${class_name}__data`}>{data}</span>
-            <h2 className={`${class_name}__title`}>{title}</h2>
+            <span className={attr ? `${class_name.split(' ')[0]}__data --${class_name.split(' ')[1]}` : `${class_name}__data`}>{data}</span>
+            <h2 className={attr ? `${class_name.split(' ')[0]}__title --${class_name.split(' ')[1]}` : `${class_name}__title`}>{title}</h2>
         </article>
     )
 };
