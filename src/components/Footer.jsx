c409957b8045c578717282f5be9c08641737aefb
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-31 10:56:46 -0400

    fix(Footer.jsx): Minor fixes added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = (props) => {
                 className="copyright">
                 <small
                     className="copyright__data">
-                    Toda la información acerca del SARS-CoV-2 (COVID-19) mostrada en esta página web es proveída directamente desde la 
-                    <a href="https://covid19.patria.org.ve/" target="_blank" rel="noopener noreferrer">API</a> pública de las estadísticas oficiales del COVID-19 
+                    Toda la información acerca del SARS-CoV-2 (COVID-19) mostrada en esta página web es proveída directamente desde la
+                     <a href="https://covid19.patria.org.ve/" target="_blank" rel="noopener noreferrer">API</a> pública de las estadísticas oficiales del COVID-19 
                     en Venezuela.
                 </small>
                 <small
