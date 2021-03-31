4fbf563cda00f2e12e1f6c20cb96cd38dab68f59
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-03-31 10:37:31 -0400

    fix(Footer.jsx): Minor typo fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = (props) => {
                 className="copyright">
                 <small
                     className="copyright__data">
-                    Toda la información acerca del SaRS-CoV-2 (COVID-19) mostrada en esta página web es proveída directamente desde la 
+                    Toda la información acerca del SARS-CoV-2 (COVID-19) mostrada en esta página web es proveída directamente desde la 
                     <a href="https://covid19.patria.org.ve/" target="_blank" rel="noopener noreferrer">API</a> pública de las estadísticas oficiales del COVID-19 
                     en Venezuela.
                 </small>
