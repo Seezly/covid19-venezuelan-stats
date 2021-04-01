ef0356515dff9e15609f5e26c3608aad81c36f74
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2021-04-01 14:03:14 -0400

    feat: Loader component and styles added

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,2 +1,26 @@
 import React from 'react';
+import SVG from './loader.svg';
 import './loader.css';
+
+const Loader = (props) => {
+    return (
+        <div 
+            className="loader">
+                <div 
+                    className="loader__svg">
+                    <img 
+                        src={SVG} 
+                        alt=""/>
+                </div>
+                <div 
+                    className="loader__text">
+                        <span 
+                            className="loader__text__span">
+                            ¡Estamos obteniendo datos!
+                        </span>
+                </div>
+        </div>
+    )
+};
+
+export default Loader;
